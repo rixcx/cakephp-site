5331c086e6568fdabe03f08d529bc4660017acf9
rixcx <adts0415.ovo@gmail.com>
2022-02-13 18:44:49 +0900

    [index] create index

---
+++ apache/html/src/Controller/IndexesController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Indexes Controller
+ *
+ *
+ * @method \App\Model\Entity\Index[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class IndexesController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function index()
+    {
+      //NOP
+    }
+}
